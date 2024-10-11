2adab8cbda9a5f64e1ceef19070ca7505b9f8119
Joseph Yose <josephyose0@gmail.com>
2024-10-11 20:55:15 +0700

    Bug removed

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -3,6 +3,7 @@
 import Form from "@components/Form";
 import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation"; // Corrected import
+import { Suspense } from "react/cjs/react.development";
 
 const EditPrompt = () => {
   const router = useRouter();
